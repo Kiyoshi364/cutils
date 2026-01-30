a548bc495b093d37b7c718e22cfac39f523c0d57
Shikiyo <shikiyo@alpine>
2026-01-30 17:44:41 +0000

    build.sh: use -Wpedantic

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 COMPILER_WARNS="-Warith-conversion -Wcast-align=strict -Wcast-qual -Wshadow -Wstrict-prototypes -Wundef -Wall -Wextra -Wstrict-overflow=5 -Wswitch-enum"
-COMPILER_PEDANTIC="-Wattributes -Wimplicit-int -Wimplicit-function-declaration -Wincompatible-pointer-types -Wint-conversion -Wmain -Wpointer-arith -Wpointer-sign -Wincompatible-pointer-types -Wwrite-strings"
-# COMPILER_PEDANTIC="-Wpedantic"
+# COMPILER_PEDANTIC="-Wattributes -Wimplicit-int -Wimplicit-function-declaration -Wincompatible-pointer-types -Wint-conversion -Wmain -Wpointer-arith -Wpointer-sign -Wincompatible-pointer-types -Wwrite-strings"
+COMPILER_PEDANTIC="-Wpedantic"
 COMPILER_FLAGS="-std=c99 ${COMPILER_PEDANTIC} ${COMPILER_WARNS} -Werror"
 
 set -xe
